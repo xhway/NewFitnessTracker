20a86231e8b163cda409efe70f513aa23a98c5a8
Xhway <dhway20@gmail.com>
2023-05-23 22:28:25 -0400

    working on schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,21 +5,21 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate('thoughts');
+      return User.find().populate('cardio resistance');
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
+      return User.findOne({ username }).populate('cardio resistance');
     },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
+    // thoughts: async (parent, { username }) => {
+    //   const params = username ? { username } : {};
+    //   return Thought.find(params).sort({ createdAt: -1 });
+    // },
+    // thought: async (parent, { thoughtId }) => {
+    //   return Thought.findOne({ _id: thoughtId });
+    // },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
+        return User.findOne({ _id: context.user._id }).populate('cardio resistance');
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -48,25 +48,24 @@ const resolvers = {
 
       return { token, user };
     },
-    addThought: async (parent, { thoughtText }, context) => {
+    addCardio: async (parent,args, context) => {
       if (context.user) {
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
+        const cardio = await Cardio.create({
+          args
         });
 
         await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
+          { $addToSet: { cardio: cardio._id } }
         );
 
-        return thought;
+        return cardio;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
+    addResistance: async (parent,args, context) => {
       if (context.user) {
-        return Thought.findOneAndUpdate(
+        return Resistance.findOneAndUpdate(
           { _id: thoughtId },
           {
             $addToSet: {
@@ -81,36 +80,47 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    removeThought: async (parent, { thoughtId }, context) => {
+    removeCardio: async (parent, args, context) => {
       if (context.user) {
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
+        const cardio = await Cardio.findOneAndDelete({
+          _id: cardioId,
+          cardioAuthor: context.user.username,
         });
 
         await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
+          { $pull: { cardio: cardio._id } }
         );
 
-        return thought;
+        return cardio;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
+    removeResistance: async (parent, args, context) => {
       if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
-          { new: true }
-        );
+          const resistance = await Resistance.findOneAndDelete({
+            _id: resistanceId,
+            resistanceAuthor: context.user.username,
+          });
+  
+          await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { resistance: resistance._id } }
+          );
+     
+      // if (context.user) {
+      //    const resistance  = await Resistance.findOneAndDelete(
+      //     { _id: resistanceId },
+      //     {
+      //       $pull: {
+      //         comments: {
+      //           _id: commentId,
+      //           commentAuthor: context.user.username,
+      //         },
+      //       },
+      //     },
+      //     { new: true }
+      //   );
       }
       throw new AuthenticationError('You need to be logged in!');
     },
